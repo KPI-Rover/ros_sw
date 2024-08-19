4d77d1015aa239dd87b4fb0339a7b5ed2dc5851a
Fastroof <50989762+Fastroof@users.noreply.github.com>
2024-08-19 10:27:11 +0000

    init commit

---
+++ launch/launch_robot.launch.py
@@ -0,0 +1,61 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, TimerAction, RegisterEventHandler
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.event_handlers import OnProcessStart
+from launch.substitutions import Command
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    package_name='apricotka-robot-car'
+
+    rsp_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name),'launch','rsp.launch.py')]
+        ),
+        launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+    )
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    controller_params = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'my_controllers.yaml'
+    )
+    controller_manager = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[{'robot_description': robot_description}, controller_params],
+    )
+    delayed_controller_manager = TimerAction(period=3.0,actions=[controller_manager])
+    delayed_diff_drive_controller = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[Node(
+                package='controller_manager',
+                executable='spawner',
+                arguments=['diff_drive_controller'],
+            )],
+        )
+    )
+    delayed_joint_broad_broadcaster = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[Node(
+                package='controller_manager',
+                executable='spawner',
+                arguments=['joint_state_broadcaster'],
+            )],
+        )
+    )
+
+    # Launch them all!
+    ld = LaunchDescription()
+    ld.add_action(rsp_cmd)
+    ld.add_action(delayed_controller_manager)
+    ld.add_action(delayed_diff_drive_controller)
+    ld.add_action(delayed_joint_broad_broadcaster)
+    return ld
---
+++ launch/launch_sim.launch.py
@@ -0,0 +1,74 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    package_name='apricotka-robot-car'
+    ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    x_pose = LaunchConfiguration('x_pose', default='0.0')
+    y_pose = LaunchConfiguration('y_pose', default='0.0')
+    
+    world = os.path.join(
+        get_package_share_directory(package_name),
+        'worlds',
+        'empty.world'
+    )
+
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': ['-r -s -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+    )
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': '-g -v4 '}.items()
+    )
+    rsp_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name),'launch','rsp.launch.py')]
+        ),
+        launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+    )
+    spawn_robot_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory(package_name),'launch', 'spawn_robot.launch.py')
+        ),
+        launch_arguments={
+            'x_pose': x_pose,
+            'y_pose': y_pose
+        }.items()
+    )
+
+    load_joint_state_broadcaster = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_diff_drive_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'diff_drive_base_controller'],
+        output='screen'
+    )
+
+    # Launch them all!
+    ld = LaunchDescription()
+
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+    ld.add_action(rsp_cmd)
+    ld.add_action(spawn_robot_cmd)
+    ld.add_action(load_joint_state_broadcaster)
+    ld.add_action(load_diff_drive_controller)
+
+    return ld
