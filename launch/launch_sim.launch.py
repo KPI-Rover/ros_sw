c7fffdf38f1302259799ed9b4734bdf6bafa15a8
Chernoivanenko Viktoriia <butterflylight01@gmail.com>
2025-02-04 15:30:32 +0200

    add oak forest world

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
     world = os.path.join(
         get_package_share_directory(package_name),
         'worlds',
-        'empty.world'
+        'forest.world'
     )
 
     gzserver_cmd = IncludeLaunchDescription(
