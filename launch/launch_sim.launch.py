1c8d2ff40b9440173e95d8521828581585bbf4c3
Chernoivanenko Viktoriia <butterflylight01@gmail.com>
2025-02-04 15:42:15 +0200

    Issue #11: Add rviz visualization

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,9 +4,10 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, AppendEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 def generate_launch_description():
     package_name='apricotka-robot-car'
@@ -55,6 +56,14 @@ def generate_launch_description():
         output='screen'
     )
 
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', get_package_share_directory(package_name) + '/config/view_bot.rviz'],
+        output='screen'
+    )
+
     # Launch them all!
     ld = LaunchDescription()
 
@@ -63,5 +72,6 @@ def generate_launch_description():
     ld.add_action(spawn_robot_cmd)
     ld.add_action(load_joint_state_broadcaster)
     ld.add_action(load_diff_drive_controller)
+    ld.add_action(rviz)
     
     return ld
