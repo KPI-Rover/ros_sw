059b86413894bba91c538414ec25d73a984d8914
Chernoivanenko Viktoriia <butterflylight01@gmail.com>
2025-02-05 12:46:22 +0200

    Issue #11: Change robot appearance

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess, AppendEnvironmentVariable
+from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
@@ -13,9 +13,10 @@ def generate_launch_description():
     package_name='apricotka-robot-car'
     ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
-    x_pose = LaunchConfiguration('x_pose', default='0.0')
-    y_pose = LaunchConfiguration('y_pose', default='0.0')
-    
+    x_pose = LaunchConfiguration('x_pose', default='-8.0')
+    y_pose = LaunchConfiguration('y_pose', default='1.5')
+    z_pose = LaunchConfiguration('z_pose', default='0.18')
+
     world = os.path.join(
         get_package_share_directory(package_name),
         'worlds',
@@ -40,7 +41,8 @@ def generate_launch_description():
         ),
         launch_arguments={
             'x_pose': x_pose,
-            'y_pose': y_pose
+            'y_pose': y_pose,
+            'z_pose': z_pose
         }.items()
     )
 
@@ -52,7 +54,7 @@ def generate_launch_description():
 
     load_diff_drive_controller = ExecuteProcess(
         cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'diff_drive_base_controller'],
+             'diff_drive_controller'],
         output='screen'
     )
 
